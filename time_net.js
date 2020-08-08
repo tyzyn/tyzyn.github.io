7f6b0ec3ef9fe8e08e5dfc4c83168e2a2351bd95
tyzyn <chris-tyson@outlook.com>
2020-08-08 17:03:03 +0100

    Update time_net.js

--- time_net.js
+++ time_net.js
@@ -9,8 +9,12 @@ d3.json("/sfhh@4.json", function(data) {
       element.start = new Date(element.start);
       element.end = new Date(element.end);
     });
+  
+  var times = d3.scaleTime()
+    .domain([d3.min(data.nodes, d => d.start), d3.max(data.nodes, d => d.end)])
+    .ticks(30)
+    .filter(time => data.nodes.some(d => contains(d, time)))
     
-    console.log("hello");
-    console.log(data);
+  console.log(times);
     
 });
